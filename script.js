97db07390d60904d889f1fc144e439fa92567f90
Denis Paskin <paskin.denis.00@gmail.com>
2022-06-06 20:58:31 +0200

    added the functionality of the game bot

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 'use strict';
 
 
-function gameBot(num) {
+function gameBot(num, attempt) {
+
   let askNum = prompt('Угадай число от 1 до 100');
 
 
   function bot() {
 
-
     if (askNum === null) {
       alert('Игра окончена');
-    } else if (askNum > num) {
-      alert('Загаданное число меньше');
-      askNum = prompt('Введите новый вариант');
-      bot();
-    } else if (askNum < num) {
-      alert('Загаданное число больше');
-      askNum = prompt('Введите новый вариант');
-      bot();
     } else if (!Number(askNum)) {
       alert('Введите число');
       askNum = prompt('Введите новый вариант');
       bot();
+    } else if (askNum > num) {
+      alert(`Загаданное число меньше, осталось ${--attempt}`);
+      if (attempt === 0) {
+        let confirm1 = confirm('Попытки закончились, хотите сыграть еще?');
+        if (confirm1) {
+          gameBot(74, 10);
+        }
+      }
+      askNum = prompt('Введите новый вариант');
+      bot();
+    } else if (askNum < num) {
+      alert(`Загаданное число больше, осталось ${--attempt}`);
+      if (attempt === 0) {
+        let confirm1 = confirm('Попытки закончились, хотите сыграть еще?');
+        if (confirm1) {
+          gameBot(74, 10);
+        } else {
+          return;
+        }
+      }
+      askNum = prompt('Введите новый вариант');
+      bot();
     } else if (askNum == num) {
-      alert('Поздравляю, Вы угадали!!!');
+      let confirm2 = confirm('"Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"');
+      if (confirm2) {
+        gameBot(43, 10);
+      } else {
+        return;
+      }
     }
+
 
   }
 
@@ -32,4 +52,4 @@ function gameBot(num) {
 
 }
 
-gameBot(55);
+gameBot(55, 10);
